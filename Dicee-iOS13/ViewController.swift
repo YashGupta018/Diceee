0588be37d61753954c3d990673d469109bae25f1
YashGupta018 <ygupta2001@gmail.com>
2022-11-11 22:42:59 +0530

    Tried Rolling The Dice When The Button Is Pressed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceThree: UIImageView! //IBOutlet is used were the code makes changes to the App
     @IBOutlet weak var diceFour: UIImageView! //IBOutlet is used were the code makes changes to the App
     
+    var rollDiceOne = 1
+    var rollDiceTwo = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -39,7 +42,18 @@ class ViewController: UIViewController {
         diceFour.image = #imageLiteral(resourceName: "DiceTwo")//"#imageLiteral(" This is the new method to use this feature
          */
         
+        diceOne.image = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ] [rollDiceOne]
+        diceTwo.image = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ] [rollDiceTwo]
         
+        rollDiceOne += 1
+        rollDiceTwo -= 1
+        
+        if(rollDiceOne == 5) {
+            rollDiceOne = rollDiceOne / 2
+        }
+        if(rollDiceTwo == 1) {
+            rollDiceTwo = rollDiceTwo * 3
+        }
         
     }
     
