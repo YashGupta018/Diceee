429e080f928cf19dd67350792cee29d426d3bbc6
YashGupta018 <ygupta2001@gmail.com>
2022-11-11 21:29:04 +0530

    Just made a comment out of Dice Image Changing Code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -19,19 +19,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        /* Changing the Dice Image once when the Button is Pressed
         diceTwo.image = #imageLiteral(resourceName: "DiceTwo")//"#imageLiteral(" This is the new method to use this feature
         diceThree.image = #imageLiteral(resourceName: "DiceFour")//"#imageLiteral(" This is the new method to use this feature
         diceFour.image = #imageLiteral(resourceName: "DiceThree")//"#imageLiteral(" This is the new method to use this feature
+         */
+        
+        
         
     }
     
     @IBAction func rollDiceButton(_ sender: UIButton) {
         
+        /* Changing the Dice Image once when the Button is Pressed
         print("Button Pressed")
         diceOne.image = #imageLiteral(resourceName: "DiceFour")//"#imageLiteral(" This is the new method to use this feature
         diceTwo.image = #imageLiteral(resourceName: "DiceOne")//"#imageLiteral(" This is the new method to use this feature
         diceThree.image = #imageLiteral(resourceName: "DiceThree")//"#imageLiteral(" This is the new method to use this feature
         diceFour.image = #imageLiteral(resourceName: "DiceTwo")//"#imageLiteral(" This is the new method to use this feature
+         */
+        
+        
         
     }
     
