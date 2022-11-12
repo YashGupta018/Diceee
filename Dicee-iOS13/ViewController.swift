51dabcc8987ffd8e9e8a00dc50ae55ceba626e1a
YashGupta018 <ygupta2001@gmail.com>
2022-11-12 16:08:39 +0530

    Deleted Old Comments & Added Randomisation Feature While Rolling The Dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,45 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceThree: UIImageView! //IBOutlet is used were the code makes changes to the App
     @IBOutlet weak var diceFour: UIImageView! //IBOutlet is used were the code makes changes to the App
     
-    var rollDiceOne = 1
-    var rollDiceTwo = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        /* Changing the Dice Image once when the Button is Pressed
-        diceTwo.image = #imageLiteral(resourceName: "DiceTwo")//"#imageLiteral(" This is the new method to use this feature
-        diceThree.image = #imageLiteral(resourceName: "DiceFour")//"#imageLiteral(" This is the new method to use this feature
-        diceFour.image = #imageLiteral(resourceName: "DiceThree")//"#imageLiteral(" This is the new method to use this feature
-         */
-        
-        
-        
-    }
-    
     @IBAction func rollDiceButton(_ sender: UIButton) {
         
-        /* Changing the Dice Image once when the Button is Pressed
-        print("Button Pressed")
-        diceOne.image = #imageLiteral(resourceName: "DiceFour")//"#imageLiteral(" This is the new method to use this feature
-        diceTwo.image = #imageLiteral(resourceName: "DiceOne")//"#imageLiteral(" This is the new method to use this feature
-        diceThree.image = #imageLiteral(resourceName: "DiceThree")//"#imageLiteral(" This is the new method to use this feature
-        diceFour.image = #imageLiteral(resourceName: "DiceTwo")//"#imageLiteral(" This is the new method to use this feature
-         */
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ]
         
-        diceOne.image = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ] [rollDiceOne]
-        diceTwo.image = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ] [rollDiceTwo]
-        
-        rollDiceOne += 1
-        rollDiceTwo -= 1
-        
-        if(rollDiceOne == 5) {
-            rollDiceOne = rollDiceOne / 2
-        }
-        if(rollDiceTwo == 1) {
-            rollDiceTwo = rollDiceTwo * 3
-        }
+        diceOne.image = diceArray.randomElement() //[Int.random(in: 0...5)] Alternative
+        diceTwo.image = diceArray.randomElement() //[Int.random(in: 0...5)] Alternative
         
     }
     
